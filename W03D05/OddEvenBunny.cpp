846d61772a2135dc222fd347a849a300833e0d50
Albedaran <nyantaobachan@gmail.com>
2019-09-27 14:35:37 +0200

    All Bunny Exercises Complete

---
+++ W03D05/OddEvenBunny.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+int bunnyEars(int bunny, int bunears);
+int main()
+{
+    int bunny;
+    std::cout << "Insert the number of BUNNIES" << std::endl;
+    std::cin >> bunny;
+    int bunears = 2;
+    std::cout << bunnyEars(bunny, bunears) + bunnyEars(bunny, bunears)/4 << std::endl;
+    return 0;
+}
+int bunnyEars(int bunny, int bunears) {
+    if (bunny != 0&& bunears != 0) {
+        return (bunny+bunnyEars(bunny, bunears-1));
+    } else {
+        return 0;
+    }
+}
