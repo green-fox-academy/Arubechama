9710ff36ddc633b0a752df37382641f5dc5066fa
Albedaran <nyantaobachan@gmail.com>
2019-10-21 17:04:17 +0200

    Odd or Even Exercise Complete

---
+++ W06D01/OddEven/OddEven.h
@@ -0,0 +1,12 @@
+#ifndef ODDEVEN_ODDEVEN_H
+#define ODDEVEN_ODDEVEN_H
+
+void oddoreven(int num) {
+    if(num % 2 == 0) {
+        printf("This number is even.");
+    } else {
+        printf("This number is odd.");
+    }
+}
+
+#endif //ODDEVEN_ODDEVEN_H
---
+++ W06D01/OddEven/main.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include "OddEven.h"
+
+int main() {
+    printf("Enter a number: \n");
+    int num;
+    scanf("%d", &num);
+    oddoreven(num);
+
+    return 0;
+}
