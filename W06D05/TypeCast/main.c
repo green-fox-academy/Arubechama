2c49974a2f39d54178539efca95e9161acf38e20
Albedaran <nyantaobachan@gmail.com>
2019-10-25 16:11:57 +0200

    Type Casting Exercises - correct values, speed and such - completed. All included in the same executable.

---
+++ W06D05/TypeCast/main.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#define DISTANCE 345
+
+// Modifiy this program to print out the number we want
+
+/* int main()
+{
+    printf("%d\n", 65); //Done.
+    return 0;
+} */
+
+/* int main()
+{
+    int seconds = 17;
+    float speed = DISTANCE / seconds;
+
+    printf("%.2f", (float)speed);
+
+    return 0;
+} */
+
+/* int main()
+{
+    int array[] = {103, 114, 101, 101, 110, 32, 102, 111, 120};
+    for (int i = 0; i < 9; ++i) {
+        printf("%c", array[i]);
+    }
+
+    return 0;
+} */
