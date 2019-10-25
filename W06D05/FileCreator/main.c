39d3e8652636fdf7e0619bf59ed386f1553b7b5c
Albedaran <nyantaobachan@gmail.com>
2019-10-25 13:31:43 +0200

    First Exercise - Create a file and read it to the console - Completed

---
+++ W06D05/FileCreator/main.c
@@ -0,0 +1,45 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#define DATA_SIZE 100
+
+int main()
+{
+    //CREATE THE FILE
+    char data[DATA_SIZE];
+
+    FILE * fPtr;
+    fPtr = fopen("file.txt", "w");
+    if(fPtr == NULL) {
+        printf("Unable to create file.\n");
+        exit(EXIT_FAILURE);
+    }
+
+    printf("Write stuff in the file: \n");
+    fgets(data, DATA_SIZE, stdin);
+    fputs(data, fPtr);
+
+    fclose(fPtr);
+
+    printf("File created.\n");
+
+    //STORE THE FILE
+    char charr;
+
+    fPtr = fopen("file.txt", "r");
+
+    if(fPtr == NULL) {
+        printf("Unable to open file.\n");
+        printf("Please check whether file exists and you have read privilege.\n");
+        exit(EXIT_FAILURE);
+    }
+
+    do {
+        charr = fgetc(fPtr);
+        putchar(charr);
+    } while(charr != EOF);
+
+    fclose(fPtr);
+
+    return 0;
+}
