49bc5323b1bece28215d01950bbd57a50b20265c
Albedaran <nyantaobachan@gmail.com>
2019-10-25 16:37:12 +0200

    Created Person Struct for the file manipulation exercise.

---
+++ W06D05/PersonStructure/main.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdint.h>
+#define MAX_CHARRAY_SIZE 64
+
+struct Person {
+    char name[MAX_CHARRAY_SIZE];
+    int age;
+    float height;
+} person;
+
+int main() {
+
+    return 0;
+}
